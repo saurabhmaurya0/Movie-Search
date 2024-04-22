f201a9c0d6eeb782deeb632689361c6598941ba9
Saurabh Maurya <sm6811940@gmail.com>
2024-04-23 00:00:42 +0530

    Update

--- js/script.js
+++ js/script.js
@@ -6,10 +6,7 @@ const SEARCHAPI =
     "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
     
-
-
-
-const moiveBox = document.querySelector("#movie-box")
+const movieBox = document.querySelector("#movie-box")
 const getMovies = async (url) => {
     const response = await fetch(url)
     const data = await response.json()
